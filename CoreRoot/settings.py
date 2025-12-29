90bf6fc66cbfff922b04021f86589facb0d3fa4b
Mrityunjoy Bhaduree <mrityunjoy.zicore@gmail.com>
2025-12-29 18:06:18 +0600

    update setting to add users app and add auth users.

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -38,8 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd party app
+    'rest_framework',
+
     # Local app
-    'core',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +130,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'users.User'

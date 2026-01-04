782c5c3c3543eaa1b269c21a4ce8b5040d6da827
Mrityunjoy Bhaduree <mrityunjoy.zicore@gmail.com>
2026-01-04 16:00:58 +0600

    remove some field

--- users/models.py
+++ users/models.py
@@ -1,22 +1,13 @@
-import uuid
-
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
-from django.http import Http404
-
-# def user_directory_path(instance, filename):
-#   # file will be uploaded to MEDIA/user_<id>/<filename>
-#   return "user_{0}/{1}".format(instance.public_id, filename)
 
 
-class UserManager(BaseUserManager):
-  def get_object_by_public_id(self, public_id):
-    try:
-      instance = self.get(public_id=public_id)
-      return instance
-    except (ObjectDoesNotExist, ValueError, TypeError):
-      return Http404
+from abstract.models import AbstractModel, AbstractManager
+
+
+
+
+class UserManager(BaseUserManager, AbstractManager):
 
   def create_user(self, username, email, password=None, **kwargs):
     """
@@ -65,9 +56,7 @@ class UserManager(BaseUserManager):
 
 
 
-class User(AbstractBaseUser, PermissionsMixin):
-  public_id = models.UUIDField(db_index=True, unique=True, default=uuid.uuid4,
-                               editable=False)
+class User(AbstractModel, AbstractBaseUser, PermissionsMixin):
   username = models.CharField(max_length=255, db_index=True, unique=True)
   first_name = models.CharField(max_length=255)
   last_name = models.CharField(max_length=255)
@@ -79,8 +68,6 @@ class User(AbstractBaseUser, PermissionsMixin):
   bio = models.TextField(null=True)
   avatar = models.ImageField(null=True)
 
-  created = models.DateTimeField(auto_now=True)
-  updated = models.DateTimeField(auto_now_add=True)
 
   USERNAME_FIELD = 'email'
   REQUIRED_FIELDS = ['username']

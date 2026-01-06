33f7f0dbe4fcb7093684616e9ee906f2b3cc2713
Mrityunjoy Bhaduree <mrityunjoy.zicore@gmail.com>
2026-01-06 12:10:50 +0600

    write post model

--- post/models.py
+++ post/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from abstract.models import AbstractModel, AbstractManager
+
+class PostManager(AbstractManager):
+  pass
+
+
+class Post(AbstractModel):
+  author = models.ForeignKey("users.User", on_delete=models.CASCADE)
+  body = models.TextField()
+  edited = models.BooleanField(default=False)
+
+  objects = PostManager()
+
+  def __str__(self):
+    return f"{self.author.name}"
+
+  class Meta:
+    db_table = "post"

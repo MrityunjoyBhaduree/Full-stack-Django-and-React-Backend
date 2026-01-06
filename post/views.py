6bc017f1fe18b58453284e8baed5e6327efe1efa
Mrityunjoy Bhaduree <mrityunjoy.zicore@gmail.com>
2026-01-06 12:12:07 +0600

    write serializer for post , write views for post, like, remove like and write urls

--- post/views.py
+++ post/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import action
 
-# Create your views here.
+
+from abstract.views import AbstractViewSet
+from post.models import Post
+from post.serializers import PostSerializer
+
+
+
+class PostAPIViewSet(AbstractViewSet):
+  http_method_names = ["post", "get", "put", "delete"]
+  permission_classes = [IsAuthenticated]
+  serializer_class = PostSerializer
+
+
+  def get_queryset(self):
+    return Post.objects.all()
+
+  def get_object(self):
+    obj = Post.objects.get_object_by_public_id(self.kwargs["pk"])
+    self.check_object_permissions(self.request, obj)
+    return obj
+
+  def create(self, request, *args, **kwargs):
+    serializer = self.get_serializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+  def update(self, request, *args, **kwargs):
+    instance = self.get_object()
+    serializer = self.get_serializer(
+      data=request.data, instance=instance, partial=False
+    )
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+  def destroy(self, request, *args, **kwargs):
+    instance = self.get_object()
+    if instance is not None:
+      self.perform_destroy(instance)
+      return Response({"message": "Post Deleted."}, status=status.HTTP_200_OK)
+    return Response({"message": "Unable to delete the post."},
+                    status=status.HTTP_400_BAD_REQUEST)
+
+  @action(methods=["post"], detail=True)
+  def like(self, request, *args, **kwargs):
+    post = self.get_object()
+    user = self.request.user
+
+    user.like(post)
+    serializer = self.serializer_class(post)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+  @action(methods=["post"], detail=True)
+  def remove_like(self, request, *args, **kwargs):
+    post = self.get_object()
+    user = self.request.user
+
+    user.remove_like(post)
+    serializer = self.serializer_class(post)
+    return Response(serializer.data, status=status.HTTP_200_OK)

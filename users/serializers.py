950602663acfadf60cafd8d35f219eba30d7f8b2
Mrityunjoy Bhaduree <mrityunjoy.zicore@gmail.com>
2026-01-17 16:39:11 +0600

    added to repesentation

--- users/serializers.py
+++ users/serializers.py
@@ -12,28 +12,29 @@ from abstract.serializers import AbstractSerializer
 
 class UserSerializer(AbstractSerializer):
 
-  avatar = serializers.SerializerMethodField(method_name="get_avatar")
   posts_count = serializers.SerializerMethodField(method_name="get_posts_count")
 
   @staticmethod
   def get_posts_count(obj):
     return obj.post_set.all().count()
 
-  def get_avatar(self, obj):
-    request = self.context.get("request")
-    if obj.avatar and hasattr(obj.avatar, "url"):
-      if request:
-        return request.build_absolute_uri(obj.avatar.url)
-      return obj.avatar.url
-    # fallback default
-    if request:
-      return request.build_absolute_uri(settings.DEFAULT_AVATAR_URL)
-    return settings.DEFAULT_AVATAR_URL
+
+  def to_representation(self, instance):
+    representation = super().to_representation(instance)
+    if not representation["avatar"]:
+      representation["avatar"] = settings.DEFAULT_AVATAR_URL
+      return representation
+    if settings.DEBUG: # debug enabled for dev
+      request = self.context.get("request")
+      representation["avatar"] = request.build_absolute_uri(
+        representation["avatar"]
+      )
+    return representation
 
   class Meta:
     model = User
     fields = ["id", "username", "name", "first_name", "last_name", "bio", "avatar",
-              "email", "is_active", "created", "updated", "posts_count"]
+               "email", "is_active", "created", "updated", "posts_count"]
     read_only_field = ["is_active"]
 
 
